2088ed7ada67aa538d755c4e1d8a5d0bb76283e0
mathlin <ommatte@gmail.com>
2014-10-21 17:40:38 +0200

    Removed Print functions temporarely

--- src/ras_utils.cpp
+++ src/ras_utils.cpp
@@ -1,5 +1,4 @@
 #include <ras_utils/ras_utils.h>
-#include <vector>
 
 namespace RAS_Utils
 {
@@ -8,27 +7,5 @@ namespace RAS_Utils
     {
         if (a >= 0) return 1;
         else        return -1;
-    }
-
-    void print(std::string text) {
-        ROS_INFO(text.c_str());
-    }
-
-    void print(std::string text, float value) {
-        ROS_INFO((text + ": %f").c_str(), value);
-    }
-
-    void print(std::string text, float value1, float value2) {
-        ROS_INFO((text + ": %f | %f").c_str(), value1, value2);
-    }
-
-    void print(const std::vector<String> & texts, const std::vector<float> & values) {
-        if(names.size() != values.size()) {
-            print("!!! ERROR !!! Vectors in print function have different sizes");
-            return;
-        }
-        for(int i = 0; i < names.size(); i++) {
-            print(texts[i], values[i]);
-        }
     }
 }
