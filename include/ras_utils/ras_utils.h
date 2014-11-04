f2a73434dbd0382290935043bb6f9740a5bd785b
Mathias Lindblom <ommatte@ommatte-MacBookAir.(none)>
2014-11-04 19:04:41 +0100

    Added shortSensorToDistanceInCM functionality

--- include/ras_utils/ras_utils.h
+++ include/ras_utils/ras_utils.h
@@ -1,11 +1,15 @@
+#include <vector>
+
 #ifndef RAS_UTILS_H
 #define RAS_UTILS_H
 
 namespace RAS_Utils
 {
-int sign(double a);
+    int sign(double a);
 
+    double shortSensorToDistanceInCM(int sensor_val);
 
+    double sensorToDistanceInCM(int sensor_val, std::vector<double> polynomial_coof);
 }
 
 #endif // RAS_UTILS_H
--- src/ras_utils.cpp
+++ src/ras_utils.cpp
@@ -1,4 +1,6 @@
 #include <ras_utils/ras_utils.h>
+#include <vector>
+#include <math.h>
 
 namespace RAS_Utils
 {
@@ -7,5 +9,26 @@ namespace RAS_Utils
     {
         if (a >= 0) return 1;
         else        return -1;
+    }
+
+    double sensorToDistanceInCM(int sensor_val, std::vector<double> polynomial_coof)
+    {
+        double distance = 0;
+        for(int n = 0; n < polynomial_coof.size(); n++) {
+            distance += polynomial_coof[n] * pow(sensor_val, n);
+        }
+        return distance;
+    }
+
+    double shortSensorToDistanceInCM(int sensor_val) {
+        return sensorToDistanceInCM(sensor_val, {
+                                    262.503338214074,
+                                    -4.88675316092840,
+                                    0.0415096360540543,
+                                    -0.000191007269073580,
+                                    4.90926172893023e-07,
+                                    -6.62146190833533e-10,
+                                    3.64763123318473e-13
+                                    });
     }
 }
